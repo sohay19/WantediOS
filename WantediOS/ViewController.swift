fef40d5fd45c368618c406dff4add9e98355394d
soha <sa4022@naver.com>
2023-02-23 20:03:37 +0900

    Initial Commit

---
+++ WantediOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WantediOS
+//
+//  Created by 소하 on 2023/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
